c2a45b6ca4588a63a225a4a12ca2f876d973afdc
shihchengli <scli@mit.edu>
2024-06-11 10:29:36 -0400

    pass multiple models into uncertainty predictor

--- chemprop/uncertainty/predictor.py
+++ chemprop/uncertainty/predictor.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from typing import Iterator
 
 from torch import Tensor
 
@@ -8,11 +9,11 @@ from chemprop.utils.registry import ClassRegistry
 
 
 class UncertaintyPredictor:
-    def __call__(self, batch: TrainingBatch, model: MPNN):
-        return self._calc_uncertainty(batch, model)
+    def __call__(self, batch: TrainingBatch, models: Iterator[MPNN]):
+        return self._calc_uncertainty(batch, models)
 
     @abstractmethod
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         """
         Calculate the uncalibrated uncertainties for the batch of data.
         """
@@ -24,76 +25,76 @@ UncertaintyPredictorRegistry = ClassRegistry[UncertaintyPredictor]()
 
 @UncertaintyPredictorRegistry.register("mve")
 class MVEPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("ensemble")
 class EnsemblePredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("classification")
 class ClassPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("evidential-total")
 class EvidentialTotalPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("evidential-epistemic")
 class EvidentialEpistemicPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("evidential-aleatoric")
 class EvidentialAleatoricPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("dropout")
 class DropoutPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("spectra-roundrobin")
 class RoundRobinSpectraPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("dirichlet")
 class DirichletPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("conformal-quantile-regression")
 class ConformalQuantileRegressionPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("conformal-regression")
 class ConformalRegressionPredictor(UncertaintyPredictor):
-    def _calc_uncertainty(self, batch, model) -> Tensor:
+    def _calc_uncertainty(self, batch, models) -> Tensor:
         ...
         return
