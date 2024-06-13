a479414c531f3fdcfbd1a765ff8c06bbde96d0df
shihchengli <scli@mit.edu>
2024-06-13 12:01:36 -0400

    add trainer into uncertainty predictors

--- chemprop/uncertainty/predictor.py
+++ chemprop/uncertainty/predictor.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 from typing import Iterator
 
+from lightning import pytorch as pl
 from torch import Tensor
 
 from chemprop.data import DataLoader
@@ -9,8 +10,8 @@ from chemprop.utils.registry import ClassRegistry
 
 
 class UncertaintyPredictor:
-    def __call__(self, dataloader: DataLoader, models: Iterator[MPNN]):
-        return self._calc_prediction_uncertainty(dataloader, models)
+    def __call__(self, dataloader: DataLoader, models: Iterator[MPNN], trainer: pl.Trainer):
+        return self._calc_prediction_uncertainty(dataloader, models, trainer)
 
     @abstractmethod
     def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
@@ -25,83 +26,83 @@ UncertaintyPredictorRegistry = ClassRegistry[UncertaintyPredictor]()
 
 @UncertaintyPredictorRegistry.register(None)
 class NoUncertaintyPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("mve")
 class MVEPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("ensemble")
 class EnsemblePredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("classification")
 class ClassPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("evidential-total")
 class EvidentialTotalPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("evidential-epistemic")
 class EvidentialEpistemicPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("evidential-aleatoric")
 class EvidentialAleatoricPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("dropout")
 class DropoutPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("spectra-roundrobin")
 class RoundRobinSpectraPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("dirichlet")
 class DirichletPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("conformal-quantile-regression")
 class ConformalQuantileRegressionPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
 
 
 @UncertaintyPredictorRegistry.register("conformal-regression")
 class ConformalRegressionPredictor(UncertaintyPredictor):
-    def _calc_prediction_uncertainty(self, dataloader, models) -> Tensor:
+    def _calc_prediction_uncertainty(self, dataloader, models, trainer) -> Tensor:
         ...
         return
